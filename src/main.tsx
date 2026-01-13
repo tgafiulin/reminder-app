50ff207ad95354921e847a1cc7815f67c8e8a8c1
tgafiulin <t.gafiulin@ya.ru>
2026-01-14 00:55:28 +0300

    Добавлен запрос разрешения notification

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,16 @@ import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 
+if ("Notification" in window && Notification.permission === "default") {
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      console.log("Разрешение на уведомления получено");
+    } else {
+      console.log("Разрешение на уведомления отклонено");
+    }
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
